393449b41e2a5c80aa41afd54516f86a553879df
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-04-08 18:21:56 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
   // Create a prepared statement with our query
-  const ps = context.env.sc-e98d-d1.prepare("SELECT * from users");
+  const ps = context.env.sc-e98d-d1.prepare("SELECT * from items");
   const data = await ps.first();
 
   return Response.json(data);
